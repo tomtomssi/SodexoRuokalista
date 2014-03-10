e4987f10c6fcad834df3e550da2879f20343676e
Tatu Solla <tatu.solla@live.com>
2014-03-10 14:55:42 +0200

    Initial commit

---
+++ Enum.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of enum
+ *
+ * @author Tatu
+ */
+abstract class Enum {
+
+    const META = 'meta';
+    
+    const GEN_DAY = 'generated_timestamp';
+    const REQ_DAY = 'requested_timestamp';
+       
+    const COURSES = 'courses';
+    const CAT = 'category';
+    
+    const FI = 'title_fi';
+    const EN = 'title_en';
+    
+
+}
---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<style>
+    *{ margin: 0; padding: 0; }
+    
+    .clearfix:after {
+        content: ".";
+        display: block;
+        clear: both;
+        visibility: hidden;
+        line-height: 0;
+        height: 0;
+    }
+
+    .clearfix {
+        display: inline-block;
+    }
+
+    html[xmlns] .clearfix {
+        display: block;
+    }
+
+    * html .clearfix {
+        height: 1%;
+    }
+    ul { margin: 0; padding: 0; margin-left: 100px; list-style-type: none; }
+
+    #left { float: left; width: 100px; }
+    #right { float: left; }
+
+    #foodWrap { float: left; clear: both; margin-bottom: 20px; }
+
+    #wrapper{
+        margin: 0 auto;
+        width: 1000px;
+        height: 800px;
+    }
+
+    #marginDiv{
+        margin-bottom: 40px;
+    }
+</style>
+<?php
+$foodJson;
+if (!$foodJson = file_get_contents("http://www.sodexo.fi/ruokalistat/output/daily_json/43/2014/03/10/fi")) {
+    die("Invalid JSON");
+}
+require_once 'Enum.php';
+$data = json_decode($foodJson, TRUE);
+?>
+<html>
+    <head>
+        <title>Ruokalista</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <div id ="wrapper">
+            
+            <div style="clear: both;"><p>OAMK:n Kaukovainion yksikön päivän ruokalista</p></div>
+            <div id="marginDiv"></div>
+            
+            <?php
+            foreach ($data[Enum::COURSES] as $var) {
+                echo "<div id='foodWrap' class='clearfix'>";
+
+                echo "<div id='left'>";
+                echo "<span>";
+                echo $var[Enum::CAT];
+                echo "</span>";
+                echo "</div>";
+
+                echo "<div id='right'>";
+                echo "<ul>";
+                echo "<li>";
+                echo $var[Enum::FI];
+                echo "</li>";
+                echo "<li>";
+                echo $var[Enum::EN];
+                echo "</li>";
+                echo "</ul>";
+                echo "</div>";
+
+                echo "</div>";
+            }
+            ?>
+            <div id="time" style="clear: both;">
+                <p>
+                    <?php echo "Client updated at: " . gmdate("Y-m-d H:i:s", $data[Enum::META][Enum::GEN_DAY]); ?>
+                </p>
+                <p>
+                    <?php echo "Sodexo updated at: " . gmdate("Y-m-d H:i:s", $data[Enum::META][Enum::REQ_DAY]); ?>
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
