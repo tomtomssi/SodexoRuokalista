9dc170ac693bd4c753b7f4e44d9e0d06117c78d8
Tatu Solla <tatu.solla@live.com>
2014-03-15 03:10:10 +0200

    Moved printing to functions and added checks so the list isn't updated on the weekends

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 <style>
     *{ margin: 0; padding: 0; }
-    
+
     .clearfix:after {
         content: ".";
         display: block;
@@ -45,12 +45,51 @@ and open the template in the editor.
     }
 </style>
 <?php
+
+function printFood($data) {
+    foreach ($data[Enum::COURSES] as $var) {
+        echo "<div id='foodWrap' class='clearfix'>";
+
+        echo "<div id='left'>";
+        echo "<span>";
+        echo $var[Enum::CAT];
+        echo "</span>";
+        echo "</div>";
+
+        echo "<div id='right'>";
+        echo "<ul>";
+        echo "<li>";
+        echo $var[Enum::FI];
+        echo "</li>";
+        echo "<li>";
+        echo $var[Enum::EN];
+        echo "</li>";
+        echo "</ul>";
+        echo "</div>";
+
+        echo "</div>";
+    }
+}
+
+function printTimestamp($data) {
+    echo "<div id='time' style='clear: both;'>";
+    echo "<p>";
+    echo "Client updated at: " . gmdate("Y-m-d H:i:s", $data[Enum::META][Enum::GEN_DAY]);
+    echo "</p>";
+    echo "<p>";
+    echo "Sodexo updated at: " . gmdate("Y-m-d H:i:s", $data[Enum::META][Enum::REQ_DAY]);
+    echo "</p>";
+    echo "</div>";
+}
+?>
+<?php
 $foodJson;
 date_default_timezone_set("Europe/Helsinki");
 $date = date("Y/m/d");
+$day = date('l', strtotime($date));
 if (!$foodJson = file_get_contents(
         "http://www.sodexo.fi/ruokalistat/output/daily_json/43/" .
-        $date . 
+        $date .
         "/fi"
         )) {
     die("Invalid JSON");
@@ -68,42 +107,19 @@ $data = json_decode($foodJson, TRUE);
     </head>
     <body>
         <div id ="wrapper">
-            
+
             <div style="clear: both;"><p>OAMK:n Kaukovainion yksikön päivän ruokalista</p></div>
             <div id="marginDiv"></div>
-            
+
+            <?php printFood($data); ?>
             <?php
-            foreach ($data[Enum::COURSES] as $var) {
-                echo "<div id='foodWrap' class='clearfix'>";
-
-                echo "<div id='left'>";
-                echo "<span>";
-                echo $var[Enum::CAT];
-                echo "</span>";
-                echo "</div>";
-
-                echo "<div id='right'>";
-                echo "<ul>";
-                echo "<li>";
-                echo $var[Enum::FI];
-                echo "</li>";
-                echo "<li>";
-                echo $var[Enum::EN];
-                echo "</li>";
-                echo "</ul>";
-                echo "</div>";
-
-                echo "</div>";
+            if ($day == "Saturday" || $day == "Sunday") {
+                echo "Ruokalista päivitetään arkisin ma-pe";
+            } else {
+                printTimestamp($data);
             }
             ?>
-            <div id="time" style="clear: both;">
-                <p>
-                    <?php echo "Client updated at: " . gmdate("Y-m-d H:i:s", $data[Enum::META][Enum::GEN_DAY]); ?>
-                </p>
-                <p>
-                    <?php echo "Sodexo updated at: " . gmdate("Y-m-d H:i:s", $data[Enum::META][Enum::REQ_DAY]); ?>
-                </p>
-            </div>
+
         </div>
     </body>
 </html>
